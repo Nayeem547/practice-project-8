79f43f7894058d2975053e6c0da4d6d36490ed7e
Nayeem547 <nayennh547@gmail.com>
2023-09-27 23:30:42 +0600

    I have completed pieChart task showing the localstorage donated total  data

--- src/Component/Layout/MainLayout.jsx
+++ src/Component/Layout/MainLayout.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 const MainLayout = () => {
+    const loc = useLocation()
+    useEffect(()=>{
+        if(loc.pathname == '/' ){
+            document.title = 'Donation Home'
+        } else {
+            document.title = `Donation ${loc.pathname.replace("/", '-')} `
+        }
+    })
     return (
         <div className="" >
             <Navbar></Navbar>
--- src/Component/Statistics/Statistics.jsx
+++ src/Component/Statistics/Statistics.jsx
@@ -1,11 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { PureComponent } from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
 
 const Statistics = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+  const [donations, setDonations] = useState([]);
+  const [donationPercentage, setDonationPercentage] = useState(0);
 
+  // Read data from local storage on component mount
+  useEffect(() => {
+    const localDonations = JSON.parse(localStorage.getItem("donation")) || [];
+    setDonations(localDonations);
+  }, []);
+  console.log(donations.length);
+  const data = [
+    { name: "Group A", value: 12 - donations.length },
+    { name: "Group B", value: donations.length },
+  ];
+
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+
+  const COLORS = ["red", "#00C49F"];
+
+  // Calculate donation percentage
+  useEffect(() => {
+    const totalDonations = donations.length;
+    const totalData = 12; // Assuming a total of 12 data points
+    const percentage = (totalDonations / totalData) * 100;
+    setDonationPercentage(percentage);
+  }, [donations]);
+  return (
+    <div>
+    <div className=" flex justify-center  " >
+      {" "}
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+            label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+
+      
+    </div>
+    <div className="flex justify-center gap-3 " >
+    
+       
+        <h2>Your Donation</h2>
+        <div className=" bg-[#00C49F] border px-14 rounded " ></div>
+        
+        
+        <h2>Total Donation</h2>
+        <div className=" bg-[#FF444A] border px-14 rounded " ></div>
+        
+      </div>
+    </div>
+  );
+};
 export default Statistics;
